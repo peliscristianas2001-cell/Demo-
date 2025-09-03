5ba9992bea3aac9bda2a10e6e0b64eb7a582cbc8
PelisCristianas <peliscristianas2001@gmail.com>
2025-09-03 20:46:37 +0000

    que use nomas esa clave api, no te preocupes, despues yo la borro

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,15 @@
+'use-strict';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
