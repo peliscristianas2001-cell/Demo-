d8aa09623557733965e19988afd7b60fb0738b13
PelisCristianas <peliscristianas2001@gmail.com>
2025-09-13 08:11:04 +0000

    haz que todo se pueda ver en github por favor, quiero que se vea, lo sub

---
+++ demo-app-prototype/next.config.ts
@@ -0,0 +1,45 @@
+import type {NextConfig} from 'next';
+
+const repo = 'demo-app-prototype';
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'instagram.fepa9-2.fna.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
+
+// Forcing a cache refresh - Change this value to force a refresh: 2
+export default nextConfig;
